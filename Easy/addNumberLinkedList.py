ee558b04732eeab857b2dc356ad9cf8a5cf15484
Qismeeee <leanhquana3bff@gmail.com>
2024-12-03 22:41:23 +0700

    Linked List Basic

--- Easy/addNumberLinkedList.py
+++ Easy/addNumberLinkedList.py
@@ -1,31 +1,55 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        current = dummy
+        carry = 0
 
-node = Node(10)
-print(node.data)
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + carry
+            carry = total // 10  # Giá trị nhớ
+            current.next = ListNode(total % 10)
+            current = current.next
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
-    def append(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
+        return dummy.next
 
-ll = LinkedList()
 
-ll.append(10)
-ll.append(20)
+def create_linked_list(values):
+    dummy = ListNode()
+    current = dummy
+    for val in values:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
 
-current = ll.head
-while current:
-    print(current.data)
-    current = current.next
+
+def print_linked_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+
+# Test cases
+l1 = create_linked_list([2, 4, 3])  
+l2 = create_linked_list([5, 6, 4])  
+
+solution = Solution()
+result = solution.addTwoNumbers(l1, l2)
+print(print_linked_list(result))  
