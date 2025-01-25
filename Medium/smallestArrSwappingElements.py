b0b77c082ecbd560a214fe35f2b749f5d97b760c
Qismeeee <leanhquana3bff@gmail.com>
2025-01-25 09:17:36 +0700

    Make Lexicographically smallest array by swapping elements test cases

--- Medium/smallestArrSwappingElements.py
+++ Medium/smallestArrSwappingElements.py
@@ -39,3 +39,34 @@ class Solution(object):
                 result[idx] = value
         
         return result
+
+# Test Case 1:
+nums1 = [1, 5, 3, 9, 8]
+limit1 = 2
+solution = Solution()
+print(solution.lexicographicallySmallestArray(nums1, limit1))  # Expected Output: [1, 3, 5, 8, 9]
+
+# Test Case 2:
+nums2 = [1, 7, 6, 18, 2, 1]
+limit2 = 3
+print(solution.lexicographicallySmallestArray(nums2, limit2))  # Expected Output: [1, 6, 7, 18, 1, 2]
+
+# Test Case 3:
+nums3 = [1, 7, 28, 19, 10]
+limit3 = 3
+print(solution.lexicographicallySmallestArray(nums3, limit3))  # Expected Output: [1, 7, 28, 19, 10]
+
+# Test Case 4:
+nums4 = [10, 20, 30, 40]
+limit4 = 15
+print(solution.lexicographicallySmallestArray(nums4, limit4))  # Expected Output: [10, 20, 30, 40]
+
+# Test Case 5:
+nums5 = [5, 5, 5, 5, 5]
+limit5 = 0
+print(solution.lexicographicallySmallestArray(nums5, limit5))  # Expected Output: [5, 5, 5, 5, 5]
+
+# Test Case 6:
+nums6 = [9, 1, 5, 7, 3]
+limit6 = 5
+print(solution.lexicographicallySmallestArray(nums6, limit6))  # Expected Output: [1, 5, 7, 9, 3]
