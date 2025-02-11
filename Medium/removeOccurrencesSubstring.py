14a73352053f443b518ca3d8788d9abd0d63dad5
Qismeeee <leanhquana3bff@gmail.com>
2025-02-11 19:47:47 +0700

    Remove All Occurrences of a Substring test cases

--- Medium/removeOccurrencesSubstring.py
+++ Medium/removeOccurrencesSubstring.py
@@ -9,3 +9,15 @@ class Solution(object):
             index = s.find(part)
             s = s[:index] + s[index + len(part):]
         return s
+    
+def test_removeOccurrences():
+    solution = Solution()
+    
+    assert solution.removeOccurrences("daabcbaabcbc", "abc") == "dab"
+    assert solution.removeOccurrences("axxxxyyyyb", "xy") == "ab"
+    assert solution.removeOccurrences("hello", "xyz") == "hello"
+    assert solution.removeOccurrences("aaaa", "a") == ""
+    assert solution.removeOccurrences("abc", "abc") == ""
+    print("All test cases passed!")
+
+test_removeOccurrences()
