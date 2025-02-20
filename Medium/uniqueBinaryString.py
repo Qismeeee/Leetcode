addc5cb8b7fdde98a30965cacddf2250ed151a14
Qismeeee <leanhquana3bff@gmail.com>
2025-02-20 22:38:43 +0700

    Find Unique Binary String

---
+++ Medium/uniqueBinaryString.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        """
+        :type nums: List[str]
+        :rtype: str
+        """
+        result = []
+        for i in range(len(nums)):
+            result.append('1' if nums[i][i] == '0' else '0')
+        return ''.join(result)
