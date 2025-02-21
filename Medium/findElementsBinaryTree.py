face35843093d495adaec9bb142a0dc94af4a589
Qismeeee <leanhquana3bff@gmail.com>
2025-02-21 22:16:11 +0700

    Find Elements in a Contaminated Binary Tree test case

---
+++ Medium/findElementsBinaryTree.py
@@ -0,0 +1,41 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class FindElements(object):
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.recovered = set()
+        self.recover(root, 0)
+    
+    def recover(self, node, value):
+        if node is None:
+            return
+        
+        node.val = value
+        self.recovered.add(value)
+        if node.left:
+            self.recover(node.left, 2 * value + 1)
+        if node.right:
+            self.recover(node.right, 2 * value + 2)
+
+    def find(self, target):
+        """
+        :type target: int
+        :rtype: bool
+        """
+        return target in self.recovered
+
+
+
+root = TreeNode(-1, TreeNode(-1, TreeNode(-1)), TreeNode(-1))
+find_elements = FindElements(root)
+print(find_elements.find(0))  
+print(find_elements.find(1))  
+print(find_elements.find(3))  
+print(find_elements.find(2)) 
+print(find_elements.find(4))  
