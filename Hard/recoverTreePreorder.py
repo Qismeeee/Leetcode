21531ccb005690cea8f0c748ba0654060285c2de
Qismeeee <leanhquana3bff@gmail.com>
2025-02-22 17:26:13 +0700

    Recover a Tree From Preorder Traversal test cases

--- Hard/recoverTreePreorder.py
+++ Hard/recoverTreePreorder.py
@@ -1,8 +1,8 @@
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution(object):
     def recoverFromPreorder(self, traversal):
@@ -34,3 +34,25 @@ class Solution(object):
                     stack[-1][1].right = node
             stack.append((depth, node))
         return stack[0][1]
+
+
+sol = Solution()
+
+# Test các test case
+test_cases = [
+    "1",
+    "1-2--3---4",
+    "1-2--3---4-5--6",
+    "1-2--3---4-5--6---7",
+    "1-401--349---90--88"
+]
+
+for case in test_cases:
+    root = sol.recoverFromPreorder(case)
+    def print_tree(node):
+        if not node:
+            return "null"
+        return str(node.val) + "," + print_tree(node.left) + "," + print_tree(node.right)
+    
+    print(f"Input: {case}")
+    print(f"Output: [{print_tree(root)}]\n")
