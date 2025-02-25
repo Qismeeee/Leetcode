ff0da8baf6bca7ba71d93deb21d3ba09e9b21377
Qismeeee <leanhquana3bff@gmail.com>
2025-02-25 23:05:25 +0700

    Number of Sub-arrays With Odd Sum test cases

--- Medium/numberOfSubarrayOddSum.py
+++ Medium/numberOfSubarrayOddSum.py
@@ -21,3 +21,33 @@ class Solution(object):
             
             result %= MOD 
         return result
+
+
+def test_numOfSubarrays():
+    solution = Solution()
+
+    # Test case 1: arr = [1, 3, 5]
+    arr = [1, 3, 5]
+    print(solution.numOfSubarrays(arr))  
+
+    # Test case 2: arr = [2, 4, 6]
+    arr = [2, 4, 6]
+    print(solution.numOfSubarrays(arr))  
+
+    # Test case 3: arr = [1, 2, 3, 4, 5, 6, 7]
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    print(solution.numOfSubarrays(arr))  
+
+    # Test case 4: arr = [1]
+    arr = [1]
+    print(solution.numOfSubarrays(arr))  
+
+    # Test case 5: arr = [2]
+    arr = [2]
+    print(solution.numOfSubarrays(arr))  
+
+    # Test case 6: arr = [1, 2, 3]
+    arr = [1, 2, 3]
+    print(solution.numOfSubarrays(arr))  
+
+test_numOfSubarrays()
