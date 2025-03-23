4c024f1c4fdeeaeeeaf0e23658516e5d7e733965
Qismeeee <leanhquana3bff@gmail.com>
2025-03-23 21:16:26 +0700

    Number of Ways to Arrive at Destination test cases

--- Medium/numberToArriveDestination.py
+++ Medium/numberToArriveDestination.py
@@ -37,3 +37,13 @@ class Solution(object):
                     ways[neighbor] = (ways[neighbor] + ways[node]) % MOD
 
         return ways[n - 1]
+
+
+s = Solution()
+n1 = 7
+roads1 = [[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
+print(s.countPaths(n1, roads1))  
+
+n2 = 2
+roads2 = [[1,0,10]]
+print(s.countPaths(n2, roads2))  
