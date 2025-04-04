1e4d948380677896f306d639700dab5bd8f2fe56
Qismeeee <leanhquana3bff@gmail.com>
2025-04-04 09:03:37 +0700

    Lowest Common Ancestor of Deepest Leaves test cases

--- Medium/lowestAncestorLeaves.py
+++ Medium/lowestAncestorLeaves.py
@@ -24,3 +24,28 @@ class Solution(object):
                 return (right_lca, right_depth + 1)
         lca, _ = dfs(root)
         return lca
+
+def test():
+    sol = Solution()
+    
+    root1 = TreeNode(3)
+    root1.left = TreeNode(5)
+    root1.right = TreeNode(1)
+    root1.left.left = TreeNode(6)
+    root1.left.right = TreeNode(2)
+    root1.right.left = TreeNode(0)
+    root1.right.right = TreeNode(8)
+    root1.left.right.left = TreeNode(7)
+    root1.left.right.right = TreeNode(4)
+    print(sol.lcaDeepestLeaves(root1).val)  
+
+    root2 = TreeNode(1)
+    print(sol.lcaDeepestLeaves(root2).val) 
+
+    root3 = TreeNode(0)
+    root3.left = TreeNode(1)
+    root3.right = TreeNode(3)
+    root3.left.right = TreeNode(2)
+    print(sol.lcaDeepestLeaves(root3).val)
+
+test()
