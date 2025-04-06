a1bbcb97edf50e47565dc934132c09426988a2e2
Qismeeee <leanhquana3bff@gmail.com>
2025-04-06 20:10:13 +0700

    Largest Divisible Subset test cases

--- Medium/largestDivisibleSubset.py
+++ Medium/largestDivisibleSubset.py
@@ -26,3 +26,20 @@ class Solution(object):
             result.append(nums[max_index])
             max_index = prev[max_index]
         return result[::-1]
+
+def test_largestDivisibleSubset():
+    solution = Solution()
+
+    nums1 = [1, 2, 3]
+    result1 = solution.largestDivisibleSubset(nums1)
+    print(f"Test Case 1: {nums1} -> Result: {result1}")
+
+    nums2 = [1, 2, 4, 8]
+    result2 = solution.largestDivisibleSubset(nums2)
+    print(f"Test Case 2: {nums2} -> Result: {result2}")
+
+    nums3 = [3, 4, 5, 6, 7, 8]
+    result3 = solution.largestDivisibleSubset(nums3)
+    print(f"Test Case 3: {nums3} -> Result: {result3}")
+
+test_largestDivisibleSubset()
