f0dc07df66ae3c6f322834c91e6be6deb81c33c8
Qismeeee <leanhquana3bff@gmail.com>
2025-04-08 11:33:48 +0700

    Minimum Number of Operations to Make Elements in Array Distinct test cases

--- Easy/minNumberArrayDistinct.py
+++ Easy/minNumberArrayDistinct.py
@@ -21,3 +21,27 @@ class Solution(object):
             else:
                 nums = nums[3:]
                 operations += 1
+
+def test_minimumOperations():
+    solution = Solution()
+    nums1 = [1, 2, 3, 4, 2, 3, 3, 5, 7]
+    result1 = solution.minimumOperations(nums1)
+    print(f"Test Case 1: {nums1} -> Result: {result1}")  
+
+    nums2 = [4, 5, 6, 4, 4]
+    result2 = solution.minimumOperations(nums2)
+    print(f"Test Case 2: {nums2} -> Result: {result2}")  
+
+    nums3 = [6, 7, 8, 9]
+    result3 = solution.minimumOperations(nums3)
+    print(f"Test Case 3: {nums3} -> Result: {result3}")
+
+    nums4 = [1, 1, 1, 1, 1]
+    result4 = solution.minimumOperations(nums4)
+    print(f"Test Case 4: {nums4} -> Result: {result4}") 
+
+    nums5 = [10, 20, 30, 40]
+    result5 = solution.minimumOperations(nums5)
+    print(f"Test Case 5: {nums5} -> Result: {result5}") 
+
+test_minimumOperations()
