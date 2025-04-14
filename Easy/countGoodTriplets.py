2812ef533798458784675cc59bff5ab25a497ec9
Qismeeee <leanhquana3bff@gmail.com>
2025-04-14 22:23:37 +0700

    Count Good Triplets test cases

--- Easy/countGoodTriplets.py
+++ Easy/countGoodTriplets.py
@@ -15,3 +15,27 @@ class Solution(object):
                     if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
                         count += 1
         return count
+
+
+solution = Solution()
+
+# Test case 1
+arr1 = [3, 0, 1, 1, 9, 7]
+a1, b1, c1 = 7, 2, 3
+print(solution.countGoodTriplets(arr1, a1, b1, c1))  
+
+# Test case 2
+arr2 = [1, 1, 2, 2, 3]
+a2, b2, c2 = 0, 0, 1
+print(solution.countGoodTriplets(arr2, a2, b2, c2))  
+
+# Test case 3
+arr3 = [5, 3, 6, 2, 4, 7]
+a3, b3, c3 = 3, 2, 3
+print(solution.countGoodTriplets(arr3, a3, b3, c3)) 
+
+# Test case 4
+arr4 = [10, 20, 30]
+a4, b4, c4 = 5, 5, 5
+print(solution.countGoodTriplets(arr4, a4, b4, c4))  
+
