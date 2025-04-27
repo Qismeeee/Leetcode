b420871d4ba42c73b8f3011af9e3d44f8785c56a
Qismeeee <leanhquana3bff@gmail.com>
2025-04-27 15:41:20 +0700

    Count Subarrays of Length Three With a Condition test cases

--- Easy/countLengthSubarray.py
+++ Easy/countLengthSubarray.py
@@ -12,3 +12,11 @@ class Solution(object):
             if first + third == middle / 2.0:
                 count += 1
         return count
+
+s = Solution()
+
+print(s.countSubarrays([1,2,1,4,1]))  
+print(s.countSubarrays([1,1,1]))      
+print(s.countSubarrays([2,4,2,8,2])) 
+print(s.countSubarrays([0,0,0]))    
+print(s.countSubarrays([5,10,5,20,5])) 
