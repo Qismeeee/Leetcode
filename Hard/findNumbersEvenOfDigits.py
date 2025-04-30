3bfc56ec8f51b8d9ab6a409e5a02e944c851decd
Qismeeee <leanhquana3bff@gmail.com>
2025-04-30 17:13:13 +0700

    Find Numbers with Even Number of Digits test cases

--- Hard/findNumbersEvenOfDigits.py
+++ Hard/findNumbersEvenOfDigits.py
@@ -5,3 +5,9 @@ class Solution(object):
         :rtype: int
         """
         return sum(1 for n in nums if len(str(n)) % 2 == 0)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.countSubarrays([2, 1, 4, 3, 5], 10))  
+    print(s.countSubarrays([1, 1, 1], 5))  
