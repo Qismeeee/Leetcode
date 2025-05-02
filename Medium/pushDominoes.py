75a7ed0a7d724cfeba99c439da97516f97eddc7d
Qismeeee <leanhquana3bff@gmail.com>
2025-05-02 20:02:02 +0700

    Push Dominoes

---
+++ Medium/pushDominoes.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def pushDominoes(self, dominoes):
+        s = 'L' + dominoes + 'R'
+        res = list(s)
+        i = 0
+        for j in range(1, len(s)):
+            if s[j] == '.':
+                continue
+            if j - i > 1:
+                if s[i] == s[j]:
+                    for k in range(i + 1, j):
+                        res[k] = s[i]
+                elif s[i] == 'R' and s[j] == 'L':
+                    l, r = i + 1, j - 1
+                    while l < r:
+                        res[l] = 'R'
+                        res[r] = 'L'
+                        l += 1
+                        r -= 1
+            i = j
+        return ''.join(res[1:-1])
