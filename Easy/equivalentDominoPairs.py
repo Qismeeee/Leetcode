6c7e1c3bb988f72934c67ec515758c0d4c7db325
Qismeeee <leanhquana3bff@gmail.com>
2025-05-04 11:04:50 +0700

    Number of Equivalent Domino Pairs test cases

--- Easy/equivalentDominoPairs.py
+++ Easy/equivalentDominoPairs.py
@@ -9,3 +9,9 @@ class Solution(object):
             res += cnt[key]
             cnt[key] += 1
         return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6]]))
+    print(s.numEquivDominoPairs([[1,2],[1,2],[1,1],[1,2],[2,2]]))
