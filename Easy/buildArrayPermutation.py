250b3d94121fe6f35fc73a91d9289c68b12fd6fe
Qismeeee <leanhquana3bff@gmail.com>
2025-05-06 22:48:45 +0700

    Build Array from Permutation test cases

--- Easy/buildArrayPermutation.py
+++ Easy/buildArrayPermutation.py
@@ -1,3 +1,8 @@
 class Solution(object):
     def buildArray(self, nums):
         return [nums[nums[i]] for i in range(len(nums))]
+    
+if __name__ == "__main__":
+    s = Solution()
+    print(s.buildArray([0,2,1,5,3,4]), "expected", [0,1,2,4,5,3])
+    print(s.buildArray([5,0,1,2,3,4]), "expected", [4,5,0,1,2,3])
