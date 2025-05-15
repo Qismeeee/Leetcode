944da8e0b5df9cc888aa3bd699875e7cb65de3cd
Qismeeee <leanhquana3bff@gmail.com>
2025-05-15 23:32:18 +0700

    Longest Unequal Adjacent Groups Subsequence I

---
+++ Easy/longestAdjacentSubsequence.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getLongestSubsequence(self, words, groups):
+        dp0, dp1 = [], []
+        for w, g in zip(words, groups):
+            if g == 0:
+                new0 = dp1 + [w]
+                if len(new0) > len(dp0):
+                    dp0 = new0
+            else:
+                new1 = dp0 + [w]
+                if len(new1) > len(dp1):
+                    dp1 = new1
+        return dp0 if len(dp0) >= len(dp1) else dp1
