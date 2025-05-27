ba2f7ac21f5591fed0c01f6f0a7faca3a0e13713
Qismeeee <leanhquana3bff@gmail.com>
2025-05-27 21:56:41 +0700

    Divisible and Non-divisible Sums Difference test cases

--- Easy/divisibleNonDivisibleSums.py
+++ Easy/divisibleNonDivisibleSums.py
@@ -4,3 +4,8 @@ class Solution(object):
         k = n // m
         divisible_sum = m * k * (k + 1) // 2
         return total - 2 * divisible_sum
+
+if __name__ == "__main__":
+    print(Solution().differenceOfSums(10, 3))  
+    print(Solution().differenceOfSums(5, 6))   
+    print(Solution().differenceOfSums(5, 1))
