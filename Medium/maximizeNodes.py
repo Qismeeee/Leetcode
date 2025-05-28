5156442bbc213d3f89a3227cba9b1bcc2189ecb7
Qismeeee <leanhquana3bff@gmail.com>
2025-05-28 15:36:58 +0700

    Maximize the Number of Target Nodes After Connecting Trees I test cases

--- Medium/maximizeNodes.py
+++ Medium/maximizeNodes.py
@@ -1,4 +1,6 @@
 from collections import deque
+import unittest
+
 class Solution(object):
     def maxTargetNodes(self, edges1, edges2, k):
         n = max(max(u, v) for u, v in edges1) + 1
@@ -32,3 +34,17 @@ class Solution(object):
         cnt2 = bfs_count(g2, m, k-1) if k > 0 else [0] * m
         best2 = max(cnt2) if k > 0 else 0
         return [cnt1[i] + best2 for i in range(n)]
+
+
+class TestMaxTargetNodes(unittest.TestCase):
+    def test_example1(self):
+        edges1 = [[0,1],[0,2],[2,3],[2,4]]
+        edges2 = [[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]]
+        self.assertEqual(Solution().maxTargetNodes(edges1, edges2, 2), [9,7,9,8,8])
+    def test_example2(self):
+        edges1 = [[0,1],[0,2],[0,3],[0,4]]
+        edges2 = [[0,1],[1,2],[2,3]]
+        self.assertEqual(Solution().maxTargetNodes(edges1, edges2, 1), [6,3,3,3,3])
+
+if __name__ == "__main__":
+    unittest.main()
