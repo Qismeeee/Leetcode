915ae42d6a2a5873c05b44c5caaddf4dfe26b0b7
Qismeeee <leanhquana3bff@gmail.com>
2025-06-08 22:56:49 +0700

    Lexicographical Numbers test cases

--- Medium/lexicographicalNumbers.py
+++ Medium/lexicographicalNumbers.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def lexicalOrder(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
         res = []
         cur = 1
         for _ in range(n):
@@ -18,3 +14,16 @@ class Solution(object):
                         cur //= 10
                     cur += 1
         return res
+
+tests = [
+    (13, [1,10,11,12,13,2,3,4,5,6,7,8,9]),
+    (2,  [1,2]),
+    (1,  [1]),
+    (20, [1,10,11,12,13,14,15,16,17,18,19,2,20,3,4,5,6,7,8,9])
+]
+
+for n, expected in tests:
+    result = Solution().lexicalOrder(n)
+    assert result == expected, f"n={n}: expected {expected}, got {result}"
+
+print("All simple tests passed")
