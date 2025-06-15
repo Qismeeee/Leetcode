dedcc6aa6077c4c488b5877e9a5a6f47c7f54198
Qismeeee <leanhquana3bff@gmail.com>
2025-06-15 16:15:37 +0700

    Max Difference You Can Get From Changing an Integer test cases

--- Medium/maxDifferenceInteger.py
+++ Medium/maxDifferenceInteger.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def maxDiff(self, num):
         s = str(num)
-        # maximize a
         a = s
         for c in s:
             if c != '9':
                 a = s.replace(c, '9')
                 break
-        # minimize b
         b = s
         if s[0] != '1':
             b = s.replace(s[0], '1')
@@ -17,3 +15,15 @@ class Solution(object):
                     b = s.replace(c, '0')
                     break
         return int(a) - int(b)
+
+if __name__ == "__main__":
+    tests = [
+        (555, 888),
+        (9, 8),
+        (123456, 820000),
+        (10000, 80000),
+        (111, 888)
+    ]
+    for num, expected in tests:
+        result = Solution().maxDiff(num)
+        print(result, expected, 'PASS' if result == expected else 'FAIL')
