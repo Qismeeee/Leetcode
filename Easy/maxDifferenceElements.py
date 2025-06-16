408fc515e338009ebef78e14fa7442da3e64be32
Qismeeee <leanhquana3bff@gmail.com>
2025-06-16 20:50:57 +0700

    Maximum Difference Between Increasing Elements test cases

--- Easy/maxDifferenceElements.py
+++ Easy/maxDifferenceElements.py
@@ -8,3 +8,16 @@ class Solution(object):
             else:
                 min_val = x
         return max_diff
+    
+if __name__ == "__main__":
+    tests = [
+        ([7, 1, 5, 4], 4),
+        ([9, 4, 3, 2], -1),
+        ([1, 5, 2, 10], 9),
+        ([2, 3], 1),
+        ([5, 5, 5], -1),
+    ]
+    for nums, expected in tests:
+        result = Solution().maximumDifference(nums)
+        status = 'PASS' if result == expected else 'FAIL'
+        print(f"nums={nums} -> {result} (expected {expected}) {status}")
