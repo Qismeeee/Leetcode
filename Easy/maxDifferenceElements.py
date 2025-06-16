3f2dbc6dba08e00a1a21416d65b2887e6954edc4
Qismeeee <leanhquana3bff@gmail.com>
2025-06-16 20:50:21 +0700

    Maximum Difference Between Increasing Elements

---
+++ Easy/maxDifferenceElements.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maximumDifference(self, nums):
+        min_val = nums[0]
+        max_diff = -1
+        for x in nums[1:]:
+            if x > min_val:
+                max_diff = max(max_diff, x - min_val)
+            else:
+                min_val = x
+        return max_diff
