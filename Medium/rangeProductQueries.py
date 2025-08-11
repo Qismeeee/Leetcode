08dc72c96e65d3cf3a4c4c8e89c08259a80a7f24
Qismeeee <leanhquana3bff@gmail.com>
2025-08-11 19:58:15 +0700

    Range Product Queries of Powers test cases

--- Medium/rangeProductQueries.py
+++ Medium/rangeProductQueries.py
@@ -23,3 +23,19 @@ class Solution(object):
             k = pref[r + 1] - pref[l]  
             ans.append(pow(2, k, MOD))
         return ans
+
+def run_tests():
+    sol = Solution()
+    cases = [
+        (15, [[0,1],[2,2],[0,3]], [2,4,64]),
+        (2, [[0,0]], [2]),
+        (1, [[0,0]], [1]),
+        (10, [[0,0],[1,1],[0,1]], [2,8,16]),
+        (37, [[0,0],[1,2],[0,2]], [1,128,128]),
+    ]
+    for n, qs, expected in cases:
+        got = sol.productQueries(n, qs)
+        assert got == expected, (n, qs, expected, got)
+    print("OK")
+
+run_tests()
