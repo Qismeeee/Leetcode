566d764d70e4546305682e6185028c88b282f077
Qismeeee <leanhquana3bff@gmail.com>
2025-08-14 10:01:20 +0700

    Largest 3-Same-Digit Number in String test cases

--- Easy/largestDigitNumber.py
+++ Easy/largestDigitNumber.py
@@ -5,3 +5,26 @@ class Solution(object):
             if t in num:
                 return t
         return ""
+
+
+def run_tests():
+    sol = Solution()
+    cases = [
+        ("6777133339", "777"),
+        ("2300019", "000"),
+        ("42352338", ""),
+        ("111", "111"),
+        ("999", "999"),
+        ("1000", "000"),
+        ("5555", "555"),
+        ("123", ""),
+        ("909090", ""),
+        ("090909000", "000"),
+        ("000", "000"),
+    ]
+    for s, expected in cases:
+        got = sol.largestGoodInteger(s)
+        assert got == expected, (s, expected, got)
+    print("OK")
+
+run_tests()
