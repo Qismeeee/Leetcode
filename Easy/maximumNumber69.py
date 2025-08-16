38bc3cfefcb86b9f578985fb31330273699bd180
Qismeeee <leanhquana3bff@gmail.com>
2025-08-16 09:08:26 +0700

    Maximum69 Number test cases

--- Easy/maximumNumber69.py
+++ Easy/maximumNumber69.py
@@ -6,3 +6,24 @@ class Solution(object):
                 s[i] = '9'
                 break
         return int(''.join(s))
+
+def run_tests():
+    sol = Solution()
+    cases = [
+        (9669, 9969),
+        (9996, 9999),
+        (9999, 9999),
+        (6, 9),
+        (9, 9),
+        (6699, 9699),
+        (9966, 9996),
+        (6969, 9969),
+        (6666, 9666),
+        (6966, 9966),
+    ]
+    for num, expected in cases:
+        got = sol.maximum69Number(num)
+        assert got == expected, (num, expected, got)
+    print("OK")
+
+run_tests()
