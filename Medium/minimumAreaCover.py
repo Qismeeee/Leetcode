8b67c466b2a87f7cff98e784a6ae8d9e84724e6b
Qismeeee <leanhquana3bff@gmail.com>
2025-08-22 13:16:02 +0700

    Find the Minimum Area to Cover All Ones I

---
+++ Medium/minimumAreaCover.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumArea(self, grid):
+        m, n = len(grid), len(grid[0])
+        min_r, min_c, max_r, max_c = m, n, -1, -1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    if i < min_r: min_r = i
+                    if j < min_c: min_c = j
+                    if i > max_r: max_r = i
+                    if j > max_c: max_c = j
+        return (max_r - min_r + 1) * (max_c - min_c + 1)
