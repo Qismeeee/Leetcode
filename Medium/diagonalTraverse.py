558837a3f1fd5031ff9f1d8e9899a76ee89dba41
Qismeeee <leanhquana3bff@gmail.com>
2025-08-25 18:25:51 +0700

    Diagonal Traverse test cases

--- Medium/diagonalTraverse.py
+++ Medium/diagonalTraverse.py
@@ -15,3 +15,22 @@ class Solution(object):
             else:
                 res.extend(tmp)
         return res
+
+def run_tests():
+    sol = Solution()
+    cases = [
+        ([[1,2,3],[4,5,6],[7,8,9]], [1,2,4,7,5,3,6,8,9]),
+        ([[1,2],[3,4]], [1,2,3,4]),
+        ([[1]], [1]),
+        ([[1,2,3,4]], [1,2,3,4]),
+        ([[1],[2],[3],[4]], [1,2,3,4]),
+        ([[1,2,3],[4,5,6]], [1,2,4,5,3,6]),
+        ([[1,2],[3,4],[5,6]], [1,2,3,5,4,6]),
+        ([[-1,-2],[-3,-4]], [-1,-2,-3,-4]),
+    ]
+    for mat, expected in cases:
+        got = sol.findDiagonalOrder(mat)
+        assert got == expected, (mat, expected, got)
+    print("OK")
+
+run_tests()
