acf6f10ec5a6c19070c827a01067009e803f00c0
Qismeeee <leanhquana3bff@gmail.com>
2025-09-08 10:27:49 +0700

    Convert Integer to the Sum of Two No-Zero Integers

---
+++ Easy/convertIntegerSum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getNoZeroIntegers(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        def no_zero(x):
+            return '0' not in str(x)
+        
+        for a in range(1, n):
+            b = n - a
+            if no_zero(a) and no_zero(b):
+                return [a, b]
