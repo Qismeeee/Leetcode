67c82e1c631dd3aac1713fa862c77844aac04c38
Qismeeee <leanhquana3bff@gmail.com>
2025-09-08 10:55:55 +0700

    Convert Integer to the Sum of Two No-Zero Integers test cases

--- Easy/convertIntegerSum.py
+++ Easy/convertIntegerSum.py
@@ -1,13 +1,19 @@
 class Solution(object):
     def getNoZeroIntegers(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
         def no_zero(x):
             return '0' not in str(x)
-        
         for a in range(1, n):
             b = n - a
             if no_zero(a) and no_zero(b):
                 return [a, b]
+
+def no_zero(x):
+    return '0' not in str(x)
+
+tests = [2, 11, 20, 101, 109, 1000, 9999]
+s = Solution()
+for n in tests:
+    a, b = s.getNoZeroIntegers(n)
+    assert a + b == n
+    assert no_zero(a) and no_zero(b)
+    print(n, [a, b])
