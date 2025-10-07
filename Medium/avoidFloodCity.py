6acca009265f02a0ac42d5c8c1322e9264492e2f
Qismeeee <leanhquana3bff@gmail.com>
2025-10-07 22:43:11 +0700

    Avoid Flood in The City

---
+++ Medium/avoidFloodCity.py
@@ -0,0 +1,26 @@
+import bisect
+
+class Solution(object):
+    def avoidFlood(self, rains):
+        """
+        :type rains: List[int]
+        :rtype: List[int]
+        """
+        n = len(rains)
+        ans = [-1] * n              
+        dry_days = []                
+        last = {}                    
+        for i, lake in enumerate(rains):
+            if lake == 0:
+                ans[i] = 1
+                bisect.insort(dry_days, i)
+            else:
+                if lake in last:
+                    j = bisect.bisect_right(dry_days, last[lake])
+                    if j == len(dry_days):
+                        return []
+                    dry_idx = dry_days.pop(j)      
+                    ans[dry_idx] = lake
+                last[lake] = i
+                ans[i] = -1                      
+        return ans
