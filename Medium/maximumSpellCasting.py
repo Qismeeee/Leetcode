c2ee0aa06130f1ac95b241fe93a2b68068c361cc
Qismeeee <leanhquana3bff@gmail.com>
2025-10-11 23:03:43 +0700

    Maximum Total Damage With Spell Casting test cases

--- Medium/maximumSpellCasting.py
+++ Medium/maximumSpellCasting.py
@@ -12,3 +12,7 @@ class Solution(object):
             take = weights[i - 1] + (dp[j + 1] if j >= 0 else 0)
             dp[i] = max(dp[i - 1], take)
         return dp[-1]
+
+power = [1,1,3,4]
+s = Solution()
+print(s.maximumTotalDamage(power))
