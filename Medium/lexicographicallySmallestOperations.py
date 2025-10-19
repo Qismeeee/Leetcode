4852b22ca1973af00c591d81562f53096a52e0b6
Qismeeee <leanhquana3bff@gmail.com>
2025-10-19 20:03:46 +0700

    Lexicographically Smallest String After Applying Operations

---
+++ Medium/lexicographicallySmallestOperations.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+class Solution(object):
+    def findLexSmallestString(self, s, a, b):
+        n = len(s)
+
+        def add_op(x):
+            arr = list(x)
+            for i in range(1, n, 2):
+                arr[i] = str((int(arr[i]) + a) % 10)
+            return ''.join(arr)
+
+        def rot_op(x):
+            return x[-b:] + x[:-b]
+
+        seen = set([s])
+        q = deque([s])
+        best = s
+        while q:
+            cur = q.popleft()
+            if cur < best:
+                best = cur
+
+            x = add_op(cur)
+            if x not in seen:
+                seen.add(x)
+                q.append(x)
+
+            y = rot_op(cur)
+            if y not in seen:
+                seen.add(y)
+                q.append(y)
+
+        return best
