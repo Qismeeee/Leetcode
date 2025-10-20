1823d2f32016cf2624b92d84e09d84dd0e605196
Qismeeee <leanhquana3bff@gmail.com>
2025-10-20 08:02:59 +0700

    Final Value of Variable After Performing Operations test cases

--- Easy/valuePerforming.py
+++ Easy/valuePerforming.py
@@ -4,3 +4,13 @@ class Solution(object):
         for op in operations:
             x += 1 if op[1] == '+' else -1
         return x
+
+ops = ["--X","X++","X++"]
+s = Solution()
+print(s.finalValueAfterOperations(ops))
+
+ops = ["++X","++X","X++"]
+print(s.finalValueAfterOperations(ops))
+
+ops = ["X++","++X","--X","X--"]
+print(s.finalValueAfterOperations(ops))
