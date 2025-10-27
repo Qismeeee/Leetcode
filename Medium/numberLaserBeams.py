a44f481fb2ddce7a95d946852d49fc298f1bd578
Qismeeee <leanhquana3bff@gmail.com>
2025-10-27 19:53:41 +0700

    Number of Laser Beams in a Bank

---
+++ Medium/numberLaserBeams.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numberOfBeams(self, bank):
+        prev = 0
+        ans = 0
+        for row in bank:
+            cnt = row.count('1')
+            if cnt:
+                ans += prev * cnt
+                prev = cnt
+        return ans
