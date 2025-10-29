6691c78dd32ab251d85ea2b9750a95ad1548cb5f
Qismeeee <leanhquana3bff@gmail.com>
2025-10-29 07:55:04 +0700

    Smallest Number With All Set Bits test cases

--- Easy/smallestSetBits.py
+++ Easy/smallestSetBits.py
@@ -4,3 +4,13 @@ class Solution(object):
         while x < n:
             x = (x << 1) | 1
         return x
+
+n = 5
+s = Solution()
+print(s.smallestNumber(n))  # 7
+
+n = 10
+print(s.smallestNumber(n))  # 15
+
+n = 3
+print(s.smallestNumber(n))  # 3
