566ef29b16ba8db294b4a23bf134201e9da095b1
Qismeeee <leanhquana3bff@gmail.com>
2025-11-01 16:25:49 +0700

    Delete Nodes From Linked List Present in Array test cases

--- Medium/deleteNodesArray.py
+++ Medium/deleteNodesArray.py
@@ -3,15 +3,28 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-class Solution(object):
-    def modifiedList(self, nums, head):
-        ban = set(nums)
-        dummy = ListNode(0, head)
-        prev, cur = dummy, head
-        while cur:
-            if cur.val in ban:
-                prev.next = cur.next
-            else:
-                prev = cur
-            cur = cur.next
-        return dummy.next
+def build_list(arr):
+    dummy = ListNode()
+    cur = dummy
+    for v in arr:
+        cur.next = ListNode(v)
+        cur = cur.next
+    return dummy.next
+
+def to_list(head):
+    out = []
+    while head:
+        out.append(head.val)
+        head = head.next
+    return out
+
+s = Solution()
+
+nums = [1,2,3]; head = build_list([1,2,3,4,5])
+print(to_list(s.modifiedList(nums, head)))  # [4, 5]
+
+nums = [1]; head = build_list([1,2,1,2,1,2])
+print(to_list(s.modifiedList(nums, head)))  # [2, 2, 2]
+
+nums = [5]; head = build_list([1,2,3,4])
+print(to_list(s.modifiedList(nums, head)))  # [1, 2, 3, 4]
