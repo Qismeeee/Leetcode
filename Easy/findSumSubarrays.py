3d57304309ffea98750710ab286704873118da48
Qismeeee <leanhquana3bff@gmail.com>
2025-11-04 20:07:42 +0700

    Find X-Sum of All K-Long Subarrays I test cases

--- Easy/findSumSubarrays.py
+++ Easy/findSumSubarrays.py
@@ -17,3 +17,15 @@ class Solution(object):
                 taken += 1
             res.append(cur)
         return res
+
+
+nums = [1,1,2,2,3,4,2,3]
+k = 6
+x = 2
+s = Solution()
+print(s.findXSum(nums, k, x))  # [6, 10, 12]
+
+nums = [3,8,7,8,7,5]
+k = 2
+x = 2
+print(s.findXSum(nums, k, x))  # [11, 15, 15, 15, 12]
