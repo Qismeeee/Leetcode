e473c313d23163474d6a8b854918b85653195882
Qismeeee <leanhquana3bff@gmail.com>
2025-11-08 17:58:17 +0700

    Minimum One Bit Operations to Make Integers Zero test cases

--- Hard/minimumBitOperations.py
+++ Hard/minimumBitOperations.py
@@ -5,3 +5,9 @@ class Solution(object):
             ans ^= n
             n >>= 1
         return ans
+
+
+s = Solution()
+print(s.minimumOneBitOperations(3))  # 2
+print(s.minimumOneBitOperations(6))  # 4
+print(s.minimumOneBitOperations(0))  # 0
