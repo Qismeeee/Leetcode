ec32b83f55af5b980e58bf73deae89f861a57060
Qismeeee <leanhquana3bff@gmail.com>
2025-11-16 10:19:13 +0700

    Number of Substrings With Only 1s test cases

--- Medium/numberSubstrings.py
+++ Medium/numberSubstrings.py
@@ -14,3 +14,11 @@ class Solution(object):
             else:
                 cur = 0
         return ans % MOD
+
+
+s = Solution()
+print(s.numSub("0110111"))  # 9
+print(s.numSub("101"))      # 2
+print(s.numSub("111111"))   # 21
+print(s.numSub("0"))        # 0
+print(s.numSub("1"))        # 1
