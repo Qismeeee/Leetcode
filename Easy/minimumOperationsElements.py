bd01cc63a1e6a04b0759de0ca0656961e82e7149
Qismeeee <leanhquana3bff@gmail.com>
2025-11-22 09:33:21 +0700

    Find Minimum Operations to Make All Elements Divisible by Three test cases

--- Easy/minimumOperationsElements.py
+++ Easy/minimumOperationsElements.py
@@ -9,3 +9,12 @@ class Solution(object):
             if x % 3 != 0:
                 ops += 1
         return ops
+
+
+s = Solution()
+
+print(s.minimumOperations([1,2,3,4]))   # 3
+print(s.minimumOperations([3,6,9]))     # 0
+print(s.minimumOperations([2,2,2]))     # 3
+print(s.minimumOperations([1]))         # 1
+print(s.minimumOperations([1,4,7,10]))  # 4
