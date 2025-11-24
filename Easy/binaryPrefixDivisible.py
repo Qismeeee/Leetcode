46f43976ce66629b22b9ee859b15135a600bcc89
Qismeeee <leanhquana3bff@gmail.com>
2025-11-24 19:02:45 +0700

    Binary Prefix Divisible By 5 test cases

--- Easy/binaryPrefixDivisible.py
+++ Easy/binaryPrefixDivisible.py
@@ -10,3 +10,10 @@ class Solution(object):
             cur = ((cur << 1) + b) % 5
             res.append(cur == 0)
         return res
+
+s = Solution()
+
+print(s.prefixesDivBy5([0,1,1]))   # [True, False, False]
+print(s.prefixesDivBy5([1,1,1]))   # [False, False, False]
+print(s.prefixesDivBy5([1,0,1,0])) # [False, False, False, False]
+print(s.prefixesDivBy5([1,0,0,1,0])) # e.g., [False, False, False, False, True]
